2281a7087c5ea25d42ee9d122e69b2d3e6f5265c
JeongwooSeo <shipfriend4371@naver.com>
2025-04-07 01:02:28 +0900

    fix: 열받는 가로스크롤 문제 해결
    - footer에 absolute가 적용되어있어서 그랬던 문제

--- src/app/entities/common/Footer.tsx
+++ src/app/entities/common/Footer.tsx
@@ -1,6 +1,10 @@
 const Footer = () => {
   return (
-    <div className={"w-full absolute bottom-4 flex justify-center"}>
+    <div
+      className={
+        "w-full absolute  left-0  text-center bottom-4 flex justify-center"
+      }
+    >
       <span className={"text-neutral-400 text-sm"}>
         @Copyright {2025} Team KNUckle
       </span>
